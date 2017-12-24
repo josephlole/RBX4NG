dc189426b7dbcb6eaa0a82b469cc9829a6425be5
Joseph Quinn <34815388+josephlole@users.noreply.github.com>
2017-12-24 18:26:25 +0200

    Add files via upload

---
+++ RBX4NG/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace RBX4NG
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.bot = new System.Windows.Forms.Timer(this.components);
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 30;
+            this.listBox.Location = new System.Drawing.Point(12, 12);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(588, 424);
+            this.listBox.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(606, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(110, 45);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start Bot";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(606, 63);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(110, 45);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop Bot";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // bot
+            // 
+            this.bot.Interval = 25;
+            this.bot.Tick += new System.EventHandler(this.bot_Tick);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.Location = new System.Drawing.Point(606, 310);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(112, 105);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.Text = "This project\r\nis maintained\r\nand created by\r\njosephlole\r\non GitHub";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 424);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.listBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RBX4NG - The ultimate 4 characters name generator for ROBLOX";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer bot;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ RBX4NG/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace RBX4NG
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void bot_Tick(object sender, EventArgs e)
+        {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_";
+            Random rnd = new Random();
+            StringBuilder b = new StringBuilder(4);
+            for (int i = 0; i < 4; i++)
+            {
+                b.Append(chars[rnd.Next(chars.Length)]);
+            }
+            string result = b.ToString();
+            WebClient client = new WebClient();
+            string pager = client.DownloadString("https://www.roblox.com/UserCheck/DoesUsernameExist?username=" + result);
+            if (pager.Contains("true"))
+            {
+
+            }
+            else
+            {
+                listBox.Items.Add(result);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            bot.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            bot.Stop();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
